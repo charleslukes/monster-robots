2edc9f0596790a5b6cddafc9d915bdbdd12a02b3
Charles Chiakwa <cchiakwa@seamfix.com>
2019-12-04 02:13:59 +0100

    Updates

---
+++ precache-manifest.8373965863f8705f5a6ec9c6f1f81f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ef9b981d84338f289997793d4cd34e",
+    "url": "/monster-robots/index.html"
+  },
+  {
+    "revision": "320e919c929fc8aec2ca",
+    "url": "/monster-robots/static/css/main.4f1a8d6f.chunk.css"
+  },
+  {
+    "revision": "cd73ff5cf936c6371e73",
+    "url": "/monster-robots/static/js/2.4972c550.chunk.js"
+  },
+  {
+    "revision": "320e919c929fc8aec2ca",
+    "url": "/monster-robots/static/js/main.47b7d827.chunk.js"
+  },
+  {
+    "revision": "aa0d09d440fb247a709c",
+    "url": "/monster-robots/static/js/runtime-main.9c9f7345.js"
+  }
+]);
